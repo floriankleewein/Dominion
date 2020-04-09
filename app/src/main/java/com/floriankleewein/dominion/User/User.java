f81c87ea8150daa68466bc6cfe62f4fffcaf3081
Florian Maurer <flmaurer@icloud.com>
2020-04-09 22:02:13 +0200

    Created two Classes, one for the User and one for the Cards the User has in the game

---
+++ app/src/main/java/com/floriankleewein/dominion/User/User.java
@@ -0,0 +1,62 @@
+package com.floriankleewein.dominion.User;
+
+/**
+ * Class User
+ * The Lists should have the params Card
+ */
+
+
+public class User {
+    static long ID;
+    private long UserID;
+    private String UserName;
+    private String UserEmail;
+    private String Password;
+    private UserCards userCards;
+
+
+
+    User(String UserName, String UserEmail, String Password){
+
+        this.UserID = ID++;
+        this.UserName = UserName;
+        this.UserEmail = UserEmail;
+        this.Password = Password;
+    }
+
+    public UserCards getUserCards() {
+        return userCards;
+    }
+
+    public void setUserCards(UserCards userCards) {
+        this.userCards = userCards;
+    }
+
+    public long getUserID() {
+        return UserID;
+    }
+
+    public void setUserID(long userID) {
+        UserID = userID;
+    }
+
+    public String getUserName() {
+        return UserName;
+    }
+
+    public void setUserName(String userName) {
+        UserName = userName;
+    }
+
+    public String getUserEmail() {
+        return UserEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        UserEmail = userEmail;
+    }
+
+
+}
+
+
---
+++ app/src/main/java/com/floriankleewein/dominion/User/UserCards.java
@@ -0,0 +1,103 @@
+package com.floriankleewein.dominion.User;
+
+import java.util.Collections;
+import java.util.LinkedList;
+
+public class UserCards {
+
+    private LinkedList<Object> Deck;
+    private LinkedList<Object> HandCards;
+    private LinkedList<Object> DiscardCards;
+
+
+    public void getFirstCards(LinkedList<Object> GivenCards) {
+        this.Deck = new LinkedList<Object>();
+        this.HandCards = new LinkedList<Object>();
+        this.DiscardCards = new LinkedList<Object>();
+
+        this.Deck = GivenCards;
+        shufflesCards();
+        drawNewCards();
+    }
+
+
+    public void shufflesCards() {
+
+        Collections.shuffle(this.Deck);
+    }
+
+
+    public void drawNewCards() {
+
+        int decksize = Deck.size();
+
+        for (int i = 0; i < HandCards.size(); i++) {
+            this.DiscardCards.add(HandCards.get(i));
+        }
+
+        this.HandCards.clear();
+
+        if (decksize < 5) {
+            this.Deck = this.DiscardCards;
+            shufflesCards();
+            this.DiscardCards.clear();
+        }
+        for (int i = 4; i >= 0; i--) {
+            this.HandCards.add(this.Deck.get(i));
+        }
+        for (int i = 4; i >= 0; i--) {
+            this.Deck.remove(i);
+        }
+    }
+
+    public void playCard(Object PlayedCard) {
+
+        this.HandCards.remove(PlayedCard);
+        this.DiscardCards.add(PlayedCard);
+
+    }
+
+
+    public void addCardtoDeck(Object newCard) {
+        this.Deck.add(newCard);
+    }
+
+    public void putCardsinTheDeck(LinkedList<Object> GivenCards) {
+        /**
+         * For specific ActionCards
+         */
+    }
+
+    public void getCard(String cardName) {
+        /**
+         * For specific ActionCards
+         */
+    }
+
+
+    public LinkedList<Object> getDeck() {
+        return Deck;
+    }
+
+    public void setDeck(LinkedList<Object> deck) {
+        Deck = deck;
+    }
+
+    public LinkedList<Object> getHandCards() {
+        return HandCards;
+    }
+
+    public void setHandCards(LinkedList<Object> handCards) {
+        HandCards = handCards;
+    }
+
+    public LinkedList<Object> getDiscardCards() {
+        return DiscardCards;
+    }
+
+    public void setDiscardCards(LinkedList<Object> discardCards) {
+        DiscardCards = discardCards;
+    }
+
+}
+
