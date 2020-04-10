03ffd64da48d7dfbe6d4da5c444d3166956b5bfb
Florian Kleewein <floriankl@edu.aau.at>
2020-04-10 15:31:30 +0200

    serverconnection starts when clicking the connect button

--- app/src/main/java/com/floriankleewein/dominion/ClientConnector.java
+++ app/src/main/java/com/floriankleewein/dominion/ClientConnector.java
@@ -1,8 +1,6 @@
 package com.floriankleewein.dominion;
 
 import com.esotericsoftware.kryonet.Client;
-import com.esotericsoftware.kryonet.Connection;
-import com.esotericsoftware.kryonet.Listener;
 
 import java.io.IOException;
 
@@ -16,16 +14,7 @@ public class ClientConnector {
 
     public void connect() throws IOException {
         client.start();
-        client.connect(5000, "143.205.174.196", 53200, 53200);
-
-        client.addListener(new Listener() {
-            public void received (Connection connection, Object object) {
-                if (object instanceof SomeResponse) {
-                    SomeResponse response = (SomeResponse)object;
-                    System.out.println(response.text);
-                }
-            }
-        });
+        client.connect(5000, "143.205.174.196", 53217, 53217);
     }
 }
 
--- app/src/main/java/com/floriankleewein/dominion/MainActivity.java
+++ app/src/main/java/com/floriankleewein/dominion/MainActivity.java
@@ -1,46 +1,35 @@
 package com.floriankleewein.dominion;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-
 import java.io.IOException;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private Button btn_connect;
+public class MainActivity extends AppCompatActivity {
 
+    Button btnCon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        btn_connect = findViewById(R.id.btn_con);
-        btn_connect.setOnClickListener(this);
+        btnCon = findViewById(R.id.btn_con);
     }
 
     @Override
-    public void onClick(View view) {
-
-        switch(view.getId()) {
-            case R.id.btn_con:
-                connect();
-                break;
-        }
-    }
-
-    protected void connect() {
-        ClientConnector temp = new ClientConnector();
-        try {
-            Log.d("PRECON", "Trying to connect.");
-            temp.connect();
-            Log.d("POSTCON", "Connection successfull!");
-        } catch (IOException e) {
-            Log.d("EXCEPTION", "IOException");
-            e.printStackTrace();
-        }
+    protected void onStart() {
+        super.onStart();
+        btnCon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                ClientConnector temp = new ClientConnector();
+                try {
+                    temp.connect();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
