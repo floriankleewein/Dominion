d37365ce258e1a934cd3dfa74cc2c81f06dd1eb5
SteinerEmanu <56165957+SteinerEmanu@users.noreply.github.com>
2020-04-10 14:30:44 +0200

    Added Button Listener and Button for connecting to server

--- app/src/main/java/com/floriankleewein/dominion/ClientConnector.java
+++ app/src/main/java/com/floriankleewein/dominion/ClientConnector.java
@@ -1,6 +1,8 @@
 package com.floriankleewein.dominion;
 
 import com.esotericsoftware.kryonet.Client;
+import com.esotericsoftware.kryonet.Connection;
+import com.esotericsoftware.kryonet.Listener;
 
 import java.io.IOException;
 
@@ -14,7 +16,16 @@ public class ClientConnector {
 
     public void connect() throws IOException {
         client.start();
-        client.connect(5000, "143.205.174.196", 53217, 53217);
+        client.connect(5000, "143.205.174.196", 53200, 53200);
+
+        client.addListener(new Listener() {
+            public void received (Connection connection, Object object) {
+                if (object instanceof SomeResponse) {
+                    SomeResponse response = (SomeResponse)object;
+                    System.out.println(response.text);
+                }
+            }
+        });
     }
 }
 
--- app/src/main/java/com/floriankleewein/dominion/MainActivity.java
+++ app/src/main/java/com/floriankleewein/dominion/MainActivity.java
@@ -4,31 +4,43 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import java.io.IOException;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button btn_connect;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Test Steiner Emanuel Rep Branch
+
+        btn_connect = findViewById(R.id.btn_con);
+        btn_connect.setOnClickListener(this);
     }
 
-
     @Override
-    protected void onStart() {
-        super.onStart();
+    public void onClick(View view) {
+
+        switch(view.getId()) {
+            case R.id.btn_con:
+                connect();
+                break;
+        }
+    }
+
+    protected void connect() {
         ClientConnector temp = new ClientConnector();
         try {
-            Log.d("test1", "testmsg1");
+            Log.d("PRECON", "Trying to connect.");
             temp.connect();
-            Log.d("test2", "testmsg2");
+            Log.d("POSTCON", "Connection successfull!");
         } catch (IOException e) {
-            Log.d("test3", "testmsg3");
+            Log.d("EXCEPTION", "IOException");
             e.printStackTrace();
         }
-        Log.d("test4", "testmsg4");
     }
 }
