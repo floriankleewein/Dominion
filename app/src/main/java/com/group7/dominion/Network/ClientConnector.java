06598cdb4e2eff9544a2d901ea19cf074c90ea84
SteinerEmanu <56165957+SteinerEmanu@users.noreply.github.com>
2020-04-27 22:53:25 +0200

    Changed Server to Java lib, Modified Client and Server for Uni Server

--- app/src/main/java/com/group7/dominion/Network/ClientConnector.java
+++ app/src/main/java/com/group7/dominion/Network/ClientConnector.java
@@ -5,12 +5,13 @@ import android.util.Log;
 import com.esotericsoftware.kryonet.Client;
 import com.esotericsoftware.kryonet.Connection;
 import com.esotericsoftware.kryonet.Listener;
+import com.floriankleewein.commonclasses.Network.Game_Information;
+import com.floriankleewein.commonclasses.Network.Network_Information;
 
 import java.io.IOException;
-import java.net.InetAddress;
 
 public class ClientConnector {
-    private final String TAG = "CLIENT-CONNECTOR";
+    private final String Tag = "CLIENT-CONNECTOR"; // Debugging only
     private static final String SERVER_IP = "143.205.174.196";
     private static final int SERVER_PORT = 53217;
     private Client client;
@@ -25,15 +26,18 @@ public class ClientConnector {
 
     public void connect() {
         registerClass(MessageClass.class);
+        registerClass(Game_Information.class);
+        registerClass(Network_Information.class);
         client.start();
 
         //connects to localtestserver now
         try {
-            client.connect(5000, InetAddress.getLocalHost(), 8080);
+            //client.connect(5000, InetAddress.getLocalHost(), 8080); // local server
+            client.connect(5000, SERVER_IP, SERVER_PORT);   // Uni server
         } catch (IOException e) {
             e.printStackTrace();
         }
-        Log.d(TAG, "Connection-Status: " + client.isConnected());
+        Log.d(Tag, "Connection-Status: " + client.isConnected());
 
         MessageClass ms = new MessageClass();
         ms.setMessage("Hello Server!");
@@ -43,7 +47,7 @@ public class ClientConnector {
             public void received(Connection con, Object object) {
                 if (object instanceof MessageClass) {
                     MessageClass ms = (MessageClass) object;
-                    Log.d(TAG, "Received response: " + ms.getMessage());
+                    Log.d(Tag, "Received response: " + ms.getMessage());
                 }
             }
         });
