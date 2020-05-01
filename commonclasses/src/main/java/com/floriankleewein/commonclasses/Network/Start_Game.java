b2271914958cf13c1fe11deb38ba15d69d2ccd84
SteinerEmanu <56165957+SteinerEmanu@users.noreply.github.com>
2020-05-01 10:53:04 +0200

    Created Start_Game class

---
+++ commonclasses/src/main/java/com/floriankleewein/commonclasses/Network/Start_Game.java
@@ -0,0 +1,26 @@
+package com.floriankleewein.commonclasses.Network;
+
+import com.floriankleewein.commonclasses.Game;
+
+public class Start_Game {
+    private boolean hasGame = false;
+    private Game game;
+
+    public Game getGame() {
+        return game;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+
+    public boolean isHasGame() {
+        return hasGame;
+    }
+
+    public void setHasGame(boolean hasGame) {
+        this.hasGame = hasGame;
+    }
+
+
+}
