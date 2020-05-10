16a4d57e69f1cbb7646dcdd64b1fb043473282cc
SteinerEmanu <56165957+SteinerEmanu@users.noreply.github.com>
2020-05-10 09:08:21 +0200

    Initialized starter cards for player

--- commonclasses/src/main/java/com/floriankleewein/commonclasses/GameLogic/GameHandler.java
+++ commonclasses/src/main/java/com/floriankleewein/commonclasses/GameLogic/GameHandler.java
@@ -1,14 +1,19 @@
 package com.floriankleewein.commonclasses.GameLogic;
 
+import com.floriankleewein.commonclasses.Cards.*;
 import com.floriankleewein.commonclasses.Game;
 import com.floriankleewein.commonclasses.User.User;
+import com.floriankleewein.commonclasses.User.UserCards;
 
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class GameHandler {
-    Game game;
-    List<User> playerList = new ArrayList<>();
+    private Game game;
+    private List<User> playerList = new ArrayList<>();
+    private final int MONEY_CARDS = 7;
+    private final int ANWESEN_CARDS = 3;
 
     public GameHandler(Game game) {
         this.game = game;
@@ -16,5 +21,23 @@ public class GameHandler {
 
     public void addPlayer(User user) {
         playerList.add(user);
+    }
+
+    public void prepareGame() {
+        //TODO set starter cards for all players
+        if(playerList.size() > 1) {
+            for (User user: playerList) {
+                UserCards ucards = new UserCards();
+                for (int i = 0; i < MONEY_CARDS; i++) {
+                    Card copper = new MoneyCard(0,0, MoneyType.KUPFER);
+                    ucards.addCardtoDeck(copper);
+                }
+                for (int i = 0; i < ANWESEN_CARDS; i++) {
+                    Card anwesen = new EstateCard(2,1,EstateType.ANWESEN);
+                    ucards.addCardtoDeck(anwesen);
+                }
+                user.setUserCards(ucards);
+            }
+        }
     }
 }
