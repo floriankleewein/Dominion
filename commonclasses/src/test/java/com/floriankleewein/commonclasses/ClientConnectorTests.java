33ae2ffcc1533fa32bd885445638c807ef8eb595
Florian Kleewein <floriankl@edu.aau.at>
2020-05-18 11:08:21 +0200

    added first clientConnector tests

---
+++ commonclasses/src/test/java/com/floriankleewein/commonclasses/ClientConnectorTests.java
@@ -0,0 +1,35 @@
+package com.floriankleewein.commonclasses;
+
+import com.floriankleewein.commonclasses.Network.ClientConnector;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ClientConnectorTests {
+
+    ClientConnector clientConnector;
+
+    @Before
+    public void setup(){
+        clientConnector = ClientConnector.getClientConnector();
+    }
+
+    @Test
+    public void singletonTest(){
+        Assert.assertEquals(clientConnector, ClientConnector.getClientConnector());
+    }
+
+    @Test
+    public void connectTest(){
+        Assert.assertEquals(false, clientConnector.getClient().isConnected());
+        clientConnector.connect();
+        Assert.assertEquals(true, clientConnector.getClient().isConnected());
+    }
+
+    @After
+    public void clean(){
+        clientConnector = null;
+    }
+}
