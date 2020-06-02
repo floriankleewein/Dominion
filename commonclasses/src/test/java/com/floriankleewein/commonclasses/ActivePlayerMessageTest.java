6d227e27f8bb985c610a3dbea338143ed45d85d3
Florian Kleewein <floriankl@edu.aau.at>
2020-06-02 09:49:28 +0200

    test for ActivePlayerMessageg

---
+++ commonclasses/src/test/java/com/floriankleewein/commonclasses/ActivePlayerMessageTest.java
@@ -0,0 +1,37 @@
+package com.floriankleewein.commonclasses;
+
+import com.floriankleewein.commonclasses.network.ActivePlayerMessage;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ActivePlayerMessageTest {
+    ActivePlayerMessage msg;
+    Game game;
+
+    @Before
+    public void setup() {
+        msg = new ActivePlayerMessage();
+        game = Game.getGame();
+    }
+
+
+    @Test
+    public void testGetGame(){
+       Assert.assertEquals(null, msg.getGame());
+    }
+
+    @Test
+    public void testSetGame(){
+        msg.setGame(game);
+        Assert.assertEquals(game, msg.getGame());
+    }
+
+    @After
+    public void clean() {
+        msg = null;
+        game = null;
+    }
+}
