903018910c64e4a0a13ccb0364679016b07843d3
Florian Maurer <flmaurer@icloud.com>
2020-06-05 15:30:49 +0200

    Little fix in Activity (Missing Braces)

--- app/src/main/java/com/group7/dominion/DominionActivity.java
+++ app/src/main/java/com/group7/dominion/DominionActivity.java
@@ -193,70 +193,72 @@ public class DominionActivity extends AppCompatActivity implements ChatFragment.
                             ErrorDialogHandler errorDialogHandler = new ErrorDialogHandler();
                             errorDialogHandler.show(fragmentManager, ERRORDIALOG_CONST);
                         } else if (card instanceof ActionCard) {
-                    if (card instanceof ActionCard) {
-                        ActionCard actionCard = (ActionCard) card;
-                        switch (actionCard.getActionType()) {
-                            case HEXE:
-                                Log.i("Action", "ActionType: " + actionCard.getActionType() +
-                                        ", Card Count: " + actionCard.getAction().getCardCount() +
-                                        ", Curse Count: " + actionCard.getAction().getCurseCount());
-                                break;
-                            case WERKSTATT:
-                                Log.i("Action", "ActionType: " + actionCard.getActionType() +
-                                        ", Card Count: " + actionCard.getAction().getCardCount() +
-                                        ", Max Money Value: " + actionCard.getAction().getMaxMoneyValue());
-                                break;
-                            case SCHMIEDE:
-                                Log.i("Action", "ActionType: " + actionCard.getActionType() +
-                                        ", Card Count: " + actionCard.getAction().getCardCount());
-                                break;
-                            case MINE:
-                                Log.i("Action", "ActionType: " + actionCard.getActionType() +
-                                        ", Card Count: " + actionCard.getAction().getCardCount() +
-                                        ", Take MoneyCard That Cost Three More Than Old: " + actionCard.getAction().isTakeMoneyCardThatCostThreeMoreThanOld() +
-                                        ", Take Card On Hand: " + actionCard.getAction().isTakeCardOnHand());
-                                break;
-                            case MILIZ:
-                                Log.i("Action", "ActionType: " + actionCard.getActionType() +
-                                        ", Money Value: " + actionCard.getAction().getMoneyValue() +
-                                        ", Throw Every UserCards Until Three Left: " + actionCard.getAction().isThrowEveryUserCardsUntilThreeLeft());
-                                break;
-                            case MARKT:
-                                Log.i("Action", "ActionType: " + actionCard.getActionType() +
-                                        ", Card Count: " + actionCard.getAction().getCardCount() +
-                                        ", Action Count: " + actionCard.getAction().getActionCount() +
-                                        ", Money Value: " + actionCard.getAction().getMoneyValue() +
-                                        ", Buy Count: " + actionCard.getAction().getBuyCount());
-                                break;
-                            case KELLER:
-                                Log.i("Action", "ActionType: " + actionCard.getActionType() +
-                                        ", Action Count: " + actionCard.getAction().getActionCount() +
-                                        ", Throw Any Amount Cards: " + actionCard.getAction().isThrowAnyAmountCards());
-                                break;
-                            case HOLZFAELLER:
-                                Log.i("Action", "ActionType: " + actionCard.getActionType() +
-                                        ", Buy Count: " + actionCard.getAction().getBuyCount() +
-                                        ", Money Value: " + actionCard.getAction().getMoneyValue());
-                                break;
-                            case DORF:
-                                Log.i("Action", "ActionType: " + actionCard.getActionType() +
-                                        ", Card Count: " + actionCard.getAction().getCardCount() +
-                                        ", Action Count: " + actionCard.getAction().getActionCount());
-                                break;
-                            case BURGGRABEN:
-                                Log.i("Action", "ActionType: " + actionCard.getActionType() +
-                                        ", Card Count: " + actionCard.getAction().getCardCount() +
-                                        ", Throw Every UserCards Until Three Left: " + actionCard.getAction().isThrowEveryUserCardsUntilThreeLeft());
-                                break;
-                            default:
-                                //LKDoc: do nothing
-                                break;
+                            if (card instanceof ActionCard) {
+                                ActionCard actionCard = (ActionCard) card;
+                                switch (actionCard.getActionType()) {
+                                    case HEXE:
+                                        Log.i("Action", "ActionType: " + actionCard.getActionType() +
+                                                ", Card Count: " + actionCard.getAction().getCardCount() +
+                                                ", Curse Count: " + actionCard.getAction().getCurseCount());
+                                        break;
+                                    case WERKSTATT:
+                                        Log.i("Action", "ActionType: " + actionCard.getActionType() +
+                                                ", Card Count: " + actionCard.getAction().getCardCount() +
+                                                ", Max Money Value: " + actionCard.getAction().getMaxMoneyValue());
+                                        break;
+                                    case SCHMIEDE:
+                                        Log.i("Action", "ActionType: " + actionCard.getActionType() +
+                                                ", Card Count: " + actionCard.getAction().getCardCount());
+                                        break;
+                                    case MINE:
+                                        Log.i("Action", "ActionType: " + actionCard.getActionType() +
+                                                ", Card Count: " + actionCard.getAction().getCardCount() +
+                                                ", Take MoneyCard That Cost Three More Than Old: " + actionCard.getAction().isTakeMoneyCardThatCostThreeMoreThanOld() +
+                                                ", Take Card On Hand: " + actionCard.getAction().isTakeCardOnHand());
+                                        break;
+                                    case MILIZ:
+                                        Log.i("Action", "ActionType: " + actionCard.getActionType() +
+                                                ", Money Value: " + actionCard.getAction().getMoneyValue() +
+                                                ", Throw Every UserCards Until Three Left: " + actionCard.getAction().isThrowEveryUserCardsUntilThreeLeft());
+                                        break;
+                                    case MARKT:
+                                        Log.i("Action", "ActionType: " + actionCard.getActionType() +
+                                                ", Card Count: " + actionCard.getAction().getCardCount() +
+                                                ", Action Count: " + actionCard.getAction().getActionCount() +
+                                                ", Money Value: " + actionCard.getAction().getMoneyValue() +
+                                                ", Buy Count: " + actionCard.getAction().getBuyCount());
+                                        break;
+                                    case KELLER:
+                                        Log.i("Action", "ActionType: " + actionCard.getActionType() +
+                                                ", Action Count: " + actionCard.getAction().getActionCount() +
+                                                ", Throw Any Amount Cards: " + actionCard.getAction().isThrowAnyAmountCards());
+                                        break;
+                                    case HOLZFAELLER:
+                                        Log.i("Action", "ActionType: " + actionCard.getActionType() +
+                                                ", Buy Count: " + actionCard.getAction().getBuyCount() +
+                                                ", Money Value: " + actionCard.getAction().getMoneyValue());
+                                        break;
+                                    case DORF:
+                                        Log.i("Action", "ActionType: " + actionCard.getActionType() +
+                                                ", Card Count: " + actionCard.getAction().getCardCount() +
+                                                ", Action Count: " + actionCard.getAction().getActionCount());
+                                        break;
+                                    case BURGGRABEN:
+                                        Log.i("Action", "ActionType: " + actionCard.getActionType() +
+                                                ", Card Count: " + actionCard.getAction().getCardCount() +
+                                                ", Throw Every UserCards Until Three Left: " + actionCard.getAction().isThrowEveryUserCardsUntilThreeLeft());
+                                        break;
+                                    default:
+                                        //LKDoc: do nothing
+                                        break;
+                                }
+                                String text = "";
+                                for (User u : gameUpdateMsg1.getGame().getPlayerList()) {
+                                    text += u.getUserName() + ": " + u.getGamePoints().getWinningPoints() + "\n";
+                                }
+                                playerScores.setText(text);
+                            }
                         }
-                        String text = "";
-                        for (User u : gameUpdateMsg1.getGame().getPlayerList()) {
-                            text += u.getUserName() + ": " + u.getGamePoints().getWinningPoints() + "\n";
-                        }
-                        playerScores.setText(text);
                     }
             );
         }));
@@ -340,5 +342,4 @@ public class DominionActivity extends AppCompatActivity implements ChatFragment.
         Toast.makeText(getApplicationContext(), "Nachricht: " + msg, Toast.LENGTH_SHORT).show();
         onBackPressed();
     }
-
 }
