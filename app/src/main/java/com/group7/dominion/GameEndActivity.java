a440d33d50ef5ef935c9550e137be56d4f4016b8
Florian Maurer <flmaurer@icloud.com>
2020-06-08 19:11:41 +0200

    Fixed some smells and server must not be restarted everytime. If a game is finished, game will be setted back

--- app/src/main/java/com/group7/dominion/GameEndActivity.java
+++ app/src/main/java/com/group7/dominion/GameEndActivity.java
@@ -2,7 +2,10 @@ package com.group7.dominion;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.floriankleewein.commonclasses.network.ClientConnector;
@@ -16,11 +19,13 @@ import com.floriankleewein.commonclasses.network.ClientConnector;
 public class GameEndActivity extends AppCompatActivity {
     private String winner;
     private TextView textViewWinner;
+    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_end);
+        btn = findViewById(R.id.backToMainActivitybtn);
         textViewWinner = findViewById(R.id.winnerView);
         winner = getIntent().getStringExtra("winner");
     }
@@ -30,5 +35,12 @@ public class GameEndActivity extends AppCompatActivity {
         super.onStart();
         textViewWinner.setText(winner);
         ClientConnector.getClientConnector().sendsetGameNull();
+        Intent i = new Intent(this, MainActivity.class);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(i);
+            }
+        });
     }
 }
